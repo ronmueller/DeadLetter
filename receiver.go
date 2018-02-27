a4c746012a067ff716396074d05f663d58b6a89b
Ronald Müller <ronald.mueller@20minuten.ch>
2018-02-27 15:34:05 +0100

    Dead Letter Exchange Example

---
+++ receiver.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	// -----------------------------------------------------------
+	// Normal Message Exchange and Queue
+	err = ch.ExchangeDeclare(
+		"msgs",   // name
+		"fanout", // type
+		false,    // durable
+		false,    // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	args := make(amqp.Table)
+	args["x-dead-letter-exchange"] = "msgs.dead.letter"
+	_, err = ch.QueueDeclare(
+		"msg", // name
+		false, // durable
+		false, // delete when unused
+		false, // exclusive
+		false, // no-wait
+		args,  // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.QueueBind(
+		"msg",  // queue name
+		"#",    // routing key
+		"msgs", // exchange
+		false,
+		nil)
+	failOnError(err, "Failed to bind a queue")
+
+	// -----------------------------------------------------------
+	// Dead Letter Exchange
+	err = ch.ExchangeDeclare(
+		"msgs.dead.letter", // name
+		"fanout",           // type
+		false,              // durable
+		false,              // auto-deleted
+		false,              // internal
+		false,              // no-wait
+		nil,                // arguments
+	)
+	failOnError(err, "Failed to declare an exchange")
+
+	args = make(amqp.Table)
+	args["x-message-ttl"] = int32(5000)
+	args["x-dead-letter-exchange"] = "msgs"
+	_, err = ch.QueueDeclare(
+		"msgs.retry", // name
+		false,        // durable
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		args,         // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.QueueBind(
+		"msgs.retry",       // queue name
+		"",                 // routing key
+		"msgs.dead.letter", // exchange
+		false,
+		nil)
+	failOnError(err, "Failed to bind a queue")
+
+	// -------------------------
+	// Get Messages from normal Queue
+	msgs, err := ch.Consume(
+		"msg", // queue
+		"",    // consumer
+		false, // auto-ack
+		false, // exclusive
+		false, // no-local
+		false, // no-wait
+		nil,   // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	defer ch.Close()
+	defer conn.Close()
+
+	forever := make(chan bool)
+
+	// read message from queue
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s\n", d.Body)
+			xdeath := d.Headers["x-death"]
+
+			// check if death headers exist
+			if v, ok := d.Headers["x-death"]; ok {
+
+				// check if headerdata exist
+				if _, ok := v.([]interface{}); ok {
+
+					// read retry count from header
+					switch retry := xdeath.([]interface{})[0].(amqp.Table)["count"].(type) {
+					case int64:
+
+						// acknowledge only after 5 retries
+						if retry > int64(5) {
+							d.Ack(false)
+						} else {
+
+							// not 5 retries reached
+							// send message back to the dead letter exchange
+							fmt.Printf("%d\n\n", retry)
+							d.Nack(false, false)
+						}
+					default:
+						// if something went wrong in the header
+						// delete message
+						d.Ack(false)
+					}
+				}
+			} else {
+				// we have no header information on the first
+				// message read atempt
+				// send message to the dead letter exchange
+				d.Nack(false, false)
+			}
+
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
+
+/*
+map[
+	x-death:[
+		map[
+			queue:msgs.retry
+			reason:expired
+			routing-keys:[msg]
+			time:2018-02-26 15:37:26 +0100 CET
+			count:20
+			exchange:msgs.dead.letter
+		]
+		map[
+			routing-keys:[msg]
+			time:2018-02-26 15:37:21 +0100 CET
+			count:20
+			exchange:msgs
+			queue:msg
+			reason:rejected
+		]
+	]
+	x-first-death-exchange:msgs
+	x-first-death-queue:msg
+	x-first-death-reason:rejected
+]
+
+[map[
+	queue:msgs.retry
+	reason:expired
+	routing-keys:[msg]
+	time:2018-02-26 15:37:26 +0100 CET
+	count:22 exchange:msgs.dead.letter
+	]
+map[routing-keys:[msg] time:2018-02-26 15:37:21 +0100 CET count:22 exchange:msgs queue:msg reason:rejected]]
+
+
+
+*/
---
+++ sender.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"strconv"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	// 30 Messages with random Priorities between 0 and 9
+	for msgs := 0; msgs < 20; msgs++ {
+		prio := rand.Intn(31)
+		err = ch.Publish(
+			"msgs", // exchange
+			"msg",  // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				Headers:         amqp.Table{},
+				ContentType:     "text/plain",
+				ContentEncoding: "",
+				Body:            []byte("test" + strconv.Itoa(msgs)),
+				DeliveryMode:    amqp.Transient, // 1=non-persistent, 2=persistent
+			})
+		log.Printf(" [x] Sent %d", prio)
+		failOnError(err, "Failed to publish a message")
+	}
+}
